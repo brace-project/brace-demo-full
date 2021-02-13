dc79da5828049b14c19fc82ec3138371a2eaa420
Matthes <matthes@leuffen.de>
2021-02-13 18:19:58 +0100

    update

---
+++ app/01_init.php
@@ -0,0 +1,31 @@
+<?php
+namespace App;
+
+use Brace\Assets\AssetsMiddleware;
+use Brace\Assets\AssetsModule;
+use Brace\Core\AppLoader;
+use Brace\Core\Base\JsonReturnFormatter;
+use Brace\Core\Base\NotFoundMiddleware;
+use Brace\Core\BraceApp;
+use Brace\Mod\Request\Zend\BraceRequestZendModule;
+use Brace\Router\RouterDispatchMiddleware;
+use Brace\Router\RouterEvalMiddleware;
+use Brace\Router\RouterModule;
+use Brace\UiKit\CoreUi\CoreUiModule;
+use Brace\UiKit\CoreUi\Template\CoreUiPageReturnFormatter;
+
+AppLoader::extend(function (BraceApp $app) {
+    $app->addModule(new BraceRequestZendModule());
+    $app->addModule(new RouterModule());
+    $app->addModule(new AssetsModule());
+    $app->addModule(new CoreUiModule());
+    $app->setPipe([
+        new AssetsMiddleware(["/assets/"]),
+        new RouterEvalMiddleware(),
+        new RouterDispatchMiddleware([
+            new CoreUiPageReturnFormatter($app),
+            new JsonReturnFormatter($app)
+        ]),
+        new NotFoundMiddleware()
+    ]);
+});
--- www/index.php
+++ www/index.php
@@ -1,33 +1,10 @@
 <?php
 namespace App;
 
-
-use Brace\Core\Base\JsonReturnFormatter;
-use Brace\Core\Base\NotFoundMiddleware;
-use Brace\Core\BraceApp;
-use Brace\Mod\Request\Zend\BraceRequestZendModule;
-use Brace\Router\RouterDispatchMiddleware;
-use Brace\Router\RouterEvalMiddleware;
-use Brace\Router\RouterModule;
-use Brace\Router\Type\Route;
-use Laminas\Diactoros\Response\JsonResponse;
-
-require __DIR__ . "/../app/bootstrap.php";
-
-$app = new BraceApp();
-
-$app->addModule(new BraceRequestZendModule());
-$app->addModule(new RouterModule());
-
-$app->setPipe([
-    new RouterEvalMiddleware(),
-    new RouterDispatchMiddleware(new JsonReturnFormatter($app)),
-    new NotFoundMiddleware()
-]);
+use Brace\Core\AppLoader;
 
 
-$app->router->onGet("/", function (Route $route) {
-    return new JsonResponse(["Hello", "World", $route->requestPath]);
-});
+require __DIR__ . "/../vendor/autoload.php";
 
-$app->run();
+AppLoader::loadApp()->run();
+
